beb1b706cdcce1ed2f6dc687c69c03591accf3b9
Pierre <p.marget@yahoo.com>
2020-02-04 17:30:25 +0100

    Version collegue 1

---
+++ src/app/models/collegue.mock.ts
@@ -0,0 +1,7 @@
+import { Collegue } from '../models/Collegue';
+
+var datenaiss = new Date(15/1/2000)
+
+const mock= new Collegue ('XXX', 'MARTIN', 'Jacques', 'jm@gmail.com', datenaiss, 'https://www.unamur.be/sciences/chimie/cbo/members/membres-fichiers/anonymity.jpg/image')
+
+export {mock}
